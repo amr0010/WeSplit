8dfd21ce1b0e055befa379ae1c62b5528b627cb2
Amr Magdy <amagdy195@yahoo.com>
2021-06-18 19:08:43 +0200

    initial commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Amr Magdy on 18/06/2021.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
